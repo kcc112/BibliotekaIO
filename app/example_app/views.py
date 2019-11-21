1879eefd43911fa6b35bd2f28151ca1aa4d2c1af
Szymon Dobrowolski <216747@edu.p.lodz.pl>
2019-11-22 00:36:03 +0100

    wstepne wszystki przypadki uzycia

--- app/example_app/views.py
+++ app/example_app/views.py
@@ -1,6 +1,6 @@
-from flask import render_template, session, redirect, url_for, current_app
+from flask import render_template, request, session, redirect, url_for, current_app
 from .. import db
-from ..models import User
+from ..models import User, Book, Announcement, Event, Graphic, Borow
 from . import example_app
 from .forms import NameForm
 
@@ -23,3 +23,90 @@ def index():
     return render_template('example_app/index.html',
                            form=form, name=session.get('name'),
                            known=session.get('known', False))
+
+@example_app.route('/Books', methods=['POST','GET'])
+def books():
+    if request.method == 'POST':
+        nazwa = request.form['nazwa']
+        strony = request.form['strony']
+        nowa_ksiazka = Book(nazwa=nazwa, strony=strony)
+
+        try:
+            db.session.add(nowa_ksiazka)
+            db.session.commit()
+            return redirect("/Books")
+        except:
+            return "Problem z dodaniem ksiazki"
+    else:
+        ksiazki = Book.query.order_by(Book.id).all()
+        return render_template("/example_app/books.html",ksiazki=ksiazki)
+
+@example_app.route("/Books/delete/<int:id>")
+def deleteBook(id):
+    book_to_delete = Book.query.get_or_404(id)
+
+    try:
+        db.session.delete(book_to_delete)
+        db.session.commit()
+        return redirect("/Books")
+    except:
+        return "Blad przy usuwaniu ksiazki"
+
+@example_app.route("/Books/update/<int:id>",methods=['POST','GET'])
+def updateBook(id):
+    book_to_update = Book.query.get_or_404(id)
+
+    if request.method == 'POST':
+        if request.form['nazwa'] != "":
+            book_to_update.nazwa = request.form['nazwa']
+            book_to_update.strony = request.form['strony']
+
+            try:
+                db.session.commit()
+            except:
+                return "Blad aktualizacji ksiazki"
+        return redirect("/Books")
+    else:
+        return render_template('/example_app/update.html',ksiazka=book_to_update)
+
+@example_app.route("/Announcements", methods=['POST','GET'])
+def announcement():
+
+    if request.method == 'POST':
+        new_announcement = Announcement(nazwa=request.form['nazwa'],opis=request.form['opis'])
+        try:
+            db.session.add(new_announcement)
+            db.session.commit()
+            return redirect("/Announcements")
+        except:
+            return "Problem z dodaniem ogloszenia"
+    else:
+        announcement = Announcement.query.order_by(Announcement.id).all()
+        return render_template("/example_app/announcement.html",announcements=announcement)
+
+@example_app.route("/Graphics")
+def graphic():
+    graphic = Graphic.query.order_by(Graphic.id).all()
+    return render_template("/example_app/graphic.html", graphics=graphic)
+
+@example_app.route("/Events")
+def event():
+    event = Event.query.order_by(Event.id).all()
+    return render_template("/example_app/event.html", events=event)
+
+@example_app.route("/Borrows")
+def borrow():
+    borrow = Borow.query.order_by(Borow.id).all()
+    return render_template("/example_app/borrow.html", borrows=borrow)
+
+@example_app.route("/Borrows/delete/<int:id>")
+def deleteBorow(id):
+    borrow_to_delete = Borow.query.get_or_404(id)
+
+    try:
+        db.session.delete(borrow_to_delete)
+        db.session.commit()
+        return redirect("/Borows")
+    except:
+        return "Blad przy usuwaniu wypozyczenia"
+
--- app/models.py
+++ app/models.py
@@ -16,6 +16,56 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    # borrows = db.relationship('Borrow', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True)
+    nazwa = db.Column(db.String(64), nullable=False)
+    strony = db.Column(db.Integer, default=0)
+    # borrows = db.relationship('Borrow', backref='book', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Task %r>' % self.nazwa
+
+class Announcement(db.Model):
+    __tablename__ = 'announcements'
+    id = db.Column(db.Integer, primary_key=True)
+    nazwa = db.Column(db.String(64), nullable=False)
+    opis = db.Column(db.String(64), nullable=False)
+
+    def __repr__(self):
+        return '<Task %r>' % self.nazwa
+
+class Graphic(db.Model):
+    __tablename__ = 'graphics'
+    id = db.Column(db.Integer, primary_key=True)
+    nazwa = db.Column(db.String(64), nullable=False)
+    akcje = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<Task %r>' % self.nazwa
+
+class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    nazwa = db.Column(db.String(64), nullable=False)
+    data = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<Task %r>' % self.nazwa
+
+class Borow(db.Model):
+    __tablename__ = 'borows'
+    id = db.Column(db.Integer, primary_key=True)
+    ksiazka_id = (db.Integer)
+    # ksiazka_id = (db.Integer,db.ForeignKey('books.id'))
+    # user_id = (db.Integer,db.ForeignKey('users.id'))
+    user_id = (db.Integer)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
