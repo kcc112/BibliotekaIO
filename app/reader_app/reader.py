2599a79043e8b40fbf896388ba4dcc3716a761fe
prudowicz <przemekrudowicz@gmail.com>
2020-01-08 11:39:10 +0100

    Endpoints start with lower case

--- app/reader_app/reader.py
+++ app/reader_app/reader.py
@@ -7,15 +7,23 @@ from .books import test_add_some_books, test_add_other_data
 from .forms import ChangePasswordForm, ChangeEmailForm, BookTable, BorrowDateForm
 
 
+<<<<<<< Updated upstream
 
 @reader_app.route("/Reader")
 @login_required
+=======
+@reader_app.route("/reader")
+>>>>>>> Stashed changes
 def reader():
     return render_template('./reader/home.html', current_user=current_user)
 
 
+<<<<<<< Updated upstream
 @reader_app.route("/Reader/Books")
 @login_required
+=======
+@reader_app.route("/reader/books")
+>>>>>>> Stashed changes
 def books():
     #test_add_some_books()
     books_query = db.session.query(Book)
@@ -23,8 +31,12 @@ def books():
     return render_template('./reader/books.html') + books_table.__html__()
 
 
+<<<<<<< Updated upstream
 @reader_app.route("/Reader/Borrow", methods=['GET', 'POST'])
 @login_required
+=======
+@reader_app.route("/reader/borrow", methods=['GET', 'POST'])
+>>>>>>> Stashed changes
 def borrow():
     book = Book.query.filter_by(id=request.args["id"]).first()
     dateform = BorrowDateForm()
@@ -39,8 +51,12 @@ def borrow():
     return render_template('./reader/borrow.html', book=book, dateform=dateform)
 
 
+<<<<<<< Updated upstream
 @reader_app.route("/Reader/Borrows")
 @login_required
+=======
+@reader_app.route("/reader/borrows")
+>>>>>>> Stashed changes
 def borrows():
     user_borrows = Borrow.query.order_by(Borrow.id).filter_by(user_id=current_user.id).all()
     borrowed_books_dictionary = {}
@@ -53,29 +69,45 @@ def borrows():
     return render_template('./reader/borrows.html', borrows=user_borrows, borrowed_books=borrowed_books_dictionary)
 
 
+<<<<<<< Updated upstream
 @reader_app.route("/Reader/Events")
 @login_required
+=======
+@reader_app.route("/reader/events")
+>>>>>>> Stashed changes
 def events():
     events = Event.query.order_by(Event.id).all()
     return render_template('./reader/events.html', events=events)
 
 
+<<<<<<< Updated upstream
 @reader_app.route("/Reader/Announcements")
 @login_required
+=======
+@reader_app.route("/reader/announcements")
+>>>>>>> Stashed changes
 def announcements():
     announcements = Announcement.query.order_by(Announcement.id).all()
     return render_template('./reader/announcements.html', announcements=announcements)
 
 
+<<<<<<< Updated upstream
 @reader_app.route("/Reader/Profiles")
 @login_required
+=======
+@reader_app.route("/reader/profiles")
+>>>>>>> Stashed changes
 def profiles():
     users = User.query.order_by(User.id).all()
     return render_template('./reader/profiles.html', users=users)
 
 
+<<<<<<< Updated upstream
 @reader_app.route("/Reader/Profile/<int:id>")
 @login_required
+=======
+@reader_app.route("/reader/profile/<int:id>")
+>>>>>>> Stashed changes
 def profile(id):
     user = User.query.filter_by(id=id).first()
     user_borrows = Borrow.query.order_by(Borrow.id).filter_by(user_id=user.id).all()
@@ -88,14 +120,22 @@ def profile(id):
                            borrowed_books=borrowed_books_dictionary, user=user)
 
 
+<<<<<<< Updated upstream
 @reader_app.route("/Reader/Edit")
 @login_required
+=======
+@reader_app.route("/reader/edit")
+>>>>>>> Stashed changes
 def edit():
     return render_template('./reader/edit.html')
 
 
+<<<<<<< Updated upstream
 @reader_app.route("/Reader/Edit/Password", methods=['GET', 'POST'])
 @login_required
+=======
+@reader_app.route("/reader/edit/password", methods=['GET', 'POST'])
+>>>>>>> Stashed changes
 def change_password():
     form = ChangePasswordForm()
     if form.validate_on_submit():
@@ -111,8 +151,12 @@ def change_password():
     return render_template("reader/change_password.html", form=form)
 
 
+<<<<<<< Updated upstream
 @reader_app.route("/Reader/Edit/Email", methods=['GET', 'POST'])
 @login_required
+=======
+@reader_app.route("/reader/edit/email", methods=['GET', 'POST'])
+>>>>>>> Stashed changes
 def change_email():
     form = ChangeEmailForm()
     if form.validate_on_submit():
@@ -120,7 +164,7 @@ def change_email():
         db.session.add(current_user)
         db.session.commit()
         flash('Your Email has been updated.')
-        return redirect("/Reader/Edit/Email")
+        return redirect("/reader/edit/email")
     else:
         flash('Invalid Email.')
     return render_template("/reader/change_email.html", form=form)
