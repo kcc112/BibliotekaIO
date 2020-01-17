e15708ebeabd7f81cb73074536d19ac01b0e8f54
jklepacz98 <51494383+jklepacz98@users.noreply.github.com>
2020-01-17 23:46:30 +0100

    Resolved reader.py conflicts

--- app/reader_app/reader.py
+++ app/reader_app/reader.py
@@ -7,23 +7,12 @@ from .books import test_add_some_books, test_add_other_data
 from .forms import ChangePasswordForm, ChangeEmailForm, BookTable, BorrowDateForm
 
 
-<<<<<<< Updated upstream
-
-@reader_app.route("/Reader")
-@login_required
-=======
 @reader_app.route("/reader")
->>>>>>> Stashed changes
 def reader():
     return render_template('./reader/home.html', current_user=current_user)
 
 
-<<<<<<< Updated upstream
-@reader_app.route("/Reader/Books")
-@login_required
-=======
 @reader_app.route("/reader/books")
->>>>>>> Stashed changes
 def books():
     #test_add_some_books()
     books_query = db.session.query(Book)
@@ -31,12 +20,7 @@ def books():
     return render_template('./reader/books.html') + books_table.__html__()
 
 
-<<<<<<< Updated upstream
-@reader_app.route("/Reader/Borrow", methods=['GET', 'POST'])
-@login_required
-=======
 @reader_app.route("/reader/borrow", methods=['GET', 'POST'])
->>>>>>> Stashed changes
 def borrow():
     book = Book.query.filter_by(id=request.args["id"]).first()
     dateform = BorrowDateForm()
@@ -51,12 +35,7 @@ def borrow():
     return render_template('./reader/borrow.html', book=book, dateform=dateform)
 
 
-<<<<<<< Updated upstream
-@reader_app.route("/Reader/Borrows")
-@login_required
-=======
 @reader_app.route("/reader/borrows")
->>>>>>> Stashed changes
 def borrows():
     user_borrows = Borrow.query.order_by(Borrow.id).filter_by(user_id=current_user.id).all()
     borrowed_books_dictionary = {}
@@ -69,45 +48,25 @@ def borrows():
     return render_template('./reader/borrows.html', borrows=user_borrows, borrowed_books=borrowed_books_dictionary)
 
 
-<<<<<<< Updated upstream
-@reader_app.route("/Reader/Events")
-@login_required
-=======
 @reader_app.route("/reader/events")
->>>>>>> Stashed changes
 def events():
     events = Event.query.order_by(Event.id).all()
     return render_template('./reader/events.html', events=events)
 
 
-<<<<<<< Updated upstream
-@reader_app.route("/Reader/Announcements")
-@login_required
-=======
 @reader_app.route("/reader/announcements")
->>>>>>> Stashed changes
 def announcements():
     announcements = Announcement.query.order_by(Announcement.id).all()
     return render_template('./reader/announcements.html', announcements=announcements)
 
 
-<<<<<<< Updated upstream
-@reader_app.route("/Reader/Profiles")
-@login_required
-=======
 @reader_app.route("/reader/profiles")
->>>>>>> Stashed changes
 def profiles():
     users = User.query.order_by(User.id).all()
     return render_template('./reader/profiles.html', users=users)
 
 
-<<<<<<< Updated upstream
-@reader_app.route("/Reader/Profile/<int:id>")
-@login_required
-=======
 @reader_app.route("/reader/profile/<int:id>")
->>>>>>> Stashed changes
 def profile(id):
     user = User.query.filter_by(id=id).first()
     user_borrows = Borrow.query.order_by(Borrow.id).filter_by(user_id=user.id).all()
@@ -120,22 +79,12 @@ def profile(id):
                            borrowed_books=borrowed_books_dictionary, user=user)
 
 
-<<<<<<< Updated upstream
-@reader_app.route("/Reader/Edit")
-@login_required
-=======
 @reader_app.route("/reader/edit")
->>>>>>> Stashed changes
 def edit():
     return render_template('./reader/edit.html')
 
 
-<<<<<<< Updated upstream
-@reader_app.route("/Reader/Edit/Password", methods=['GET', 'POST'])
-@login_required
-=======
 @reader_app.route("/reader/edit/password", methods=['GET', 'POST'])
->>>>>>> Stashed changes
 def change_password():
     form = ChangePasswordForm()
     if form.validate_on_submit():
@@ -151,12 +100,7 @@ def change_password():
     return render_template("reader/change_password.html", form=form)
 
 
-<<<<<<< Updated upstream
-@reader_app.route("/Reader/Edit/Email", methods=['GET', 'POST'])
-@login_required
-=======
 @reader_app.route("/reader/edit/email", methods=['GET', 'POST'])
->>>>>>> Stashed changes
 def change_email():
     form = ChangeEmailForm()
     if form.validate_on_submit():
